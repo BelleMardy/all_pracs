a847b71f41501dfbc9c7a165eb45db517513bfbc
Belle <belle.belle@my.jcu.edu.au>
2017-08-31 09:35:40 +1000

    spacing

--- week_05/prac_5.py
+++ week_05/prac_5.py
@@ -8,7 +8,8 @@ ALL_CAPS = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Terri
             "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-STATE_NAMES = {"QLD":"Queensland", "NSW": "New South Wales", "NT" :"Northern Territory", "WA" : "Western Australia", "ACT":"Australian Capital Territory", "VIC":"Victoria", "TAS":"Tasmania"}
+STATE_NAMES = {"QLD":"Queensland", "NSW": "New South Wales", "NT" :"Northern Territory", "WA" :
+                "Western Australia", "ACT":"Australian Capital Territory", "VIC":"Victoria", "TAS":"Tasmania"}
 # print(STATE_NAMES)
 
 state = input("Enter short state: ").upper()
