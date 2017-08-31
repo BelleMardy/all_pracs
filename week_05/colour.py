b6d24f8073a092d3abd38824f692aada467a09e0
Belle <belle.belle@my.jcu.edu.au>
2017-08-31 10:08:51 +1000

    colour

---
+++ week_05/colour.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Hexadecimal colour lookup
+"""
+
+COLOUR_CODES = {"DeepSkyBlue1": "#00bfff   ", "DeepSkyBlue2": "#00b2ee",
+                "DeepSkyBlue3": "#009acd", "DeepSkyBlue4": "#00688b",
+                "DodgerBlue1": "#1e90ff", "AntiqueWhite4": "#8b8378",
+                "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
+                "aquamarine4": "#458b74", "azure1": "#f0ffff",
+                "azure2": "#e0eeee", "azure3": "#c1cdcd", "azure4": "#838b8b",
+                "beige": "#f5f5dc", "bisque1": "#ffe4c4"}
+
+colour_name = input("Enter a colour name: ")
+while colour_name != "":
+    # Note: using the get dictionary method
+    # means you will get None if the key is not found
+    print("The code for \"{}\" is {}".format(colour_name,
+                                             COLOUR_CODES.get(colour_name)))
+    colour_name = input("Enter a colour name: ")
